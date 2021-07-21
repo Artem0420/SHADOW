deb2e435a8decb51b5d56609e99dc1a73a47e158
Artem0420 <82901122+Artem0420@users.noreply.github.com>
2021-07-21 14:18:38 +0300

    init

---
+++ project1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Program
+    {
+        1488 1488 1488 1488
+        static void Main(string[] args)
+        {
+        }
+    }
+}
